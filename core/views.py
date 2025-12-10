49ee88ad3562a1be0c20cbfe9207bc692b47eed6
PREYE <akordonald.15@gmail.com>
2025-12-10 19:55:59 +0100

    homepage v1

--- core/views.py
+++ core/views.py
@@ -2,9 +2,8 @@ from django.core.cache import cache
 from rest_framework.views import APIView
 from .responses import success_response
 from menu.serializers import MenuItemSerializer
-from category.serializers import CategorySerializer
 from menu.models import MenuItem
-from category.models import Category
+
 
 CACHE_KEY = "homepage_v1"
 CACHE_TTL = 60
@@ -17,8 +16,7 @@ class HomepageAPIView(APIView):
         if cached:
             return success_response("Homepage fetched (cached)", data=cached)
 
-        categories = CategorySerializer(Category.objects.all(), many=True).data
         menu_items = MenuItemSerializer(MenuItem.objects.filter(is_available=True)[:12], many=True).data
-        data = {"categories": categories, "menu": menu_items}
+        data = {"menu": menu_items}
         cache.set(CACHE_KEY, data, timeout=CACHE_TTL)
         return success_response("Homepage fetched", data=data)
