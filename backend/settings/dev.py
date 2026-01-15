4a4b3db0099eb651c3fa73c986e0c70872167ba4
Akor Donald Ayibapreye <akordonald15@gmail.com>
2026-01-15 19:01:02 +0100

    Update email settings for development environment

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -15,14 +15,19 @@ CORS_ALLOW_CREDENTIALS = True
 SIMPLE_JWT['AUTH_COOKIE_SECURE'] = False
 SIMPLE_JWT['AUTH_COOKIE_SAMESITE'] = 'Lax'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = env.int('EMAIL_PORT', default=587)
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True) == True
-EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False) 
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='akordonald15@gmail.com')
+EMAIL_BACKEND = env(
+    "EMAIL_BACKEND",
+    default="django.core.mail.backends.console.EmailBackend"
+)
+
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="ci@example.com")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="ci-password")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="ci@example.com")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
